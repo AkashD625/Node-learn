a1d479aa9908568d846a8ea31dcc81f5880a7c45
Akash <divateakash21@gmail.com>
2024-02-05 14:59:10 +0530

    #

---
+++ event_loop.js
@@ -0,0 +1,7 @@
+/*  Event loop - Assume that you have 3 works to exicute and 1st - have some console 2nd - have some settimeout (which means -time consumming ) and the 3rd - some console's , hear the tasks  that consumes the time will be exicuted latter and other task will be excuted till that.
+
+- program will not stop till the all program get executed and other program will note get disturebed becouse of settime out programs.
+
+*/
+
+
